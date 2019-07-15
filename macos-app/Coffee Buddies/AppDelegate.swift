41240aa41c945c86b0506eb08dad8a163d1435cf
Stefan Ludwig <stefan.ludwig@slu-it.de>
2019-07-15 20:09:39 +0200

    added initial MacOS app

---
+++ macos-app/Coffee Buddies/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Coffee Buddies
+//
+//  Created by Stefan on 15.07.19.
+//  Copyright © 2019 Stefan Ludwig. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macos-app/Coffee Buddies/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Coffee Buddies
+//
+//  Created by Stefan on 15.07.19.
+//  Copyright © 2019 Stefan Ludwig. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
